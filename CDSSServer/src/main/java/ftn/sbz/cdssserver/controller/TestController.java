0d98efa76b500e71c3351ef3f2c70b7aaa4966eb
Aleksandar Savic <alek_bn@hotmail.com>
2018-08-30 00:15:08 +0200

    Add LoginDto, give up on logout implementation

--- CDSSServer/src/main/java/ftn/sbz/cdssserver/controller/TestController.java
+++ CDSSServer/src/main/java/ftn/sbz/cdssserver/controller/TestController.java
@@ -2,7 +2,6 @@ package ftn.sbz.cdssserver.controller;
 
 import ftn.sbz.cdssserver.model.User;
 import ftn.sbz.cdssserver.model.dto.CreateUserDto;
-import ftn.sbz.cdssserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,29 +17,4 @@ import javax.validation.Valid;
 @RequestMapping("/test")
 public class TestController {
 
-    @Autowired
-    UserService userService;
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @PostMapping("/admin")
-    public ResponseEntity createAdmin(@RequestBody @Valid CreateUserDto userDto) {
-        final User created = userService.register(userDto.createDoctor());
-
-        if (created == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(created.getId(), HttpStatus.CREATED);
-    }
-
-    @PreAuthorize("hasAuthority('DOCTOR')")
-    @PostMapping
-    public ResponseEntity createDoctor(@RequestBody @Valid CreateUserDto userDto) {
-        System.out.println("DOC\n\n");
-        final User created = userService.register(userDto.createDoctor());
-
-        if (created == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(created.getId(), HttpStatus.CREATED);
-    }
 }
--- CDSSServer/src/main/java/ftn/sbz/cdssserver/controller/UserController.java
+++ CDSSServer/src/main/java/ftn/sbz/cdssserver/controller/UserController.java
@@ -1,16 +1,21 @@
 package ftn.sbz.cdssserver.controller;
 
 import ftn.sbz.cdssserver.model.User;
+import ftn.sbz.cdssserver.model.dto.CreateUserDto;
+import ftn.sbz.cdssserver.model.dto.LoginDto;
 import ftn.sbz.cdssserver.security.TokenUtils;
 import ftn.sbz.cdssserver.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -28,7 +33,7 @@ public class UserController {
     }
 
     @PostMapping(value = "/login")
-    public ResponseEntity<String> login(@RequestBody User user) {
+    public ResponseEntity<String> login(@RequestBody LoginDto user) {
         String username = user.getUsername();
         System.out.println("LOGIN TRY " + username + " pass: " + user.getPassword());
 
@@ -42,6 +47,17 @@ public class UserController {
         } catch (Exception ex) {
             return new ResponseEntity<>("Login failed. Bad username or password.", HttpStatus.UNAUTHORIZED);
         }
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PostMapping("/register")
+    public ResponseEntity createAdmin(@RequestBody @Valid CreateUserDto userDto) {
+        final User created = userService.register(userDto.createDoctor());
+
+        if (created == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(created.getId(), HttpStatus.CREATED);
     }
 
 }
